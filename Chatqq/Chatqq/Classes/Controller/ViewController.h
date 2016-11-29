fb5d2a8b12a51120e8ce3c01e3c0f7b1e06e3ac9
humeng12 <936608021@qq.com>
2016-11-29 12:31:39 +0800

    提交
    项目创建

---
+++ Chatqq/Chatqq/Classes/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chatqq
+//
+//  Created by 胡猛 on 2016/11/29.
+//  Copyright © 2016年 HuMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Chatqq/Chatqq/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chatqq
+//
+//  Created by 胡猛 on 2016/11/29.
+//  Copyright © 2016年 HuMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
